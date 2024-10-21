6c3789ebd6e2e433102030663d95e4d4ff860786
Anshidtp <shaanshid07@gmail.com>
2024-10-21 22:33:11 +0530

    notebook file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'industry_safety_detector',
+    version= '0.0.0',
+    author= 'anshid',
+    author_email= 'shaanshid07@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
